0b86be950fb407afd7fdfbb2ebd75fb028bc0c8d
Sean Warman <swarman@sysadmins-MacBook-Pro.local>
2021-11-19 19:58:31 +0000

    added daemon to project

---
+++ README.md
@@ -0,0 +1,15 @@
+# Bookmarks Updater
+Add this project into your bookmarks folder that contains *Bookmarks* and *Bookmarks.bak*.
+
+Usually found under a dir called Default.
+
+Do `yarn start` to start the pm2 daemon and it'll watch that folder for changes using git status.
+
+Add the following to .zshrc or .bashrc...
+
+```
+yarn --cwd ~/path/to/Default start &> /dev/null
+```
+
+Do `yarn monit` and `yarn ps` to monitor the process or see it in a list format.
+Do `yarn stop` to stop and delete the process.
---
+++ bookmarks-daemon.js
@@ -0,0 +1,39 @@
+const { spawn } = require('child_process');
+
+function run(stdin) {
+  const [command, ...args] = stdin.split(' ')
+  const proc = spawn(command, args)
+
+  return new Promise((res, rej) => {
+    proc.stdout.on('data', (data) => {
+      res(data.toString().trim())
+    })
+
+    proc.stderr.on('data', (data) => {
+      rej(data.toString().trim())
+    })
+
+    proc.on('close', (code) => {
+      rej(`Status code ${code}`)
+    })
+  })
+}
+
+(async function worker() {
+
+  try {
+    const out = await run('git status -s')
+
+    if (out.length === 0) {
+      throw 'No output'
+    }
+
+    await run('git commit -am "updated"')
+    await run('git push -u origin master')
+
+  } catch (error) {
+    console.log(error)
+  }
+
+  setTimeout(worker, 5000)
+})()
